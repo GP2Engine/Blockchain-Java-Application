01d24f89e96ebd0f4d57a8aa17a59d697491c999
Tasos Kollias <t-tech458@hotmail.com>
2020-11-12 17:58:11 +0200

    Update BlockChain.java

--- Project Version 1/Blockchain_Single_Thread/src/BlockChain.java
+++ Project Version 1/Blockchain_Single_Thread/src/BlockChain.java
@@ -1,15 +1,6 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
-/*
-METAPTYXIAKO Prohgmena Systhmata Plhroforikhs 2019
-Ergasia: Blockchain JAVA
-Onoma: Anastasios
-Epwnymo: Kollias
-A.M: MPSP19018
-
-VERSION 1: Single Thread
- */
 
 public class BlockChain {
 
